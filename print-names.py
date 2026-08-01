52bc235255df32726620c47358cac66439b7c534
RUSH R7 <200yescas@gmail.com>
2026-08-01 12:41:02 -0700

    project-names-python

---
+++ print-names.py
@@ -0,0 +1,42 @@
+# Ejercicios básicos de python
+
+## print
+
+print("Hello world")
+print("Me encanta estudiar python")
+print('Estudiar con "Python Total" es super divertido')
+print(500+55)
+print("Hola,Mundo")
+print("¡Esto es muy fácil!")
+
+## print en números
+
+print(42)
+print(3.14159)
+print(10+5)
+print(10*5)
+print(100/4)
+
+## Línea vacía
+
+print("Primera línea")
+print()
+print("Segunda línea")
+
+## Separar con comas
+
+print("Hola",'Mundo')
+print("Hola","mundo")
+print("El resultado es:",42)
+print("Suma=",10,"+",5,"=",15)
+print("Resta=",10000,"-",5000,"=",5000)
+
+## Cambiar el seperador con sep ( separador)
+
+print("A","B","C","D",sep="-")
+print("A","B","C","D",sep="/")
+
+
+
+
+
